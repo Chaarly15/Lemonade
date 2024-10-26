a7f5184c7bdd3485dbf5d242677db2ba133da082
AMIAN ATTOBRA JEAN CHARLY <amian0071967@univmetiers.ci>
2024-10-26 08:03:35 +0000

    Spacer

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,24 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -54,7 +52,7 @@ fun LemonadePreviewApp(){
 @Composable
 fun LemonadeApp(modifier: Modifier = Modifier){
 
-    var result by remember { mutableStateOf(1) }
+    var result by remember { mutableIntStateOf(1) }
     val imageResources = when (result){
         1->R.drawable.lemon_tree
         2->R.drawable.lemon_squeeze
@@ -81,6 +79,8 @@ fun LemonadeApp(modifier: Modifier = Modifier){
 
             Image(painter = painterResource(imageResources), contentDescription = String())
         }
+
+        Spacer(modifier = Modifier.height(20.dp))
         Text(description)
     }
 }
